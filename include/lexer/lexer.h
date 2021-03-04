5a5065fff37200c339efd52135945f72104fea9a
Heggour Abdeljalil <abdeljalil.heggour@gmail.com>
2021-03-04 17:44:11 +0100

    Current token

--- include/lexer/lexer.h
+++ include/lexer/lexer.h
@@ -6,4 +6,7 @@
 // driver function to get next tokens from text
 extern token_t get_next_token();
 
+//placeholder of all the necessary information about the current token.
+extern token_t current_token;
+
 #endif
