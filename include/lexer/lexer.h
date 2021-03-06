1fb7f1c53f36ca238fc4c1abf1d29dbebee9d442
Fakhri Moud <moad_fakhri_1999@outlook.com>
2021-03-06 15:19:41 +0100

    add lexer init driver function

--- include/lexer/lexer.h
+++ include/lexer/lexer.h
@@ -2,6 +2,10 @@
 #define LEXER_H
 
 #include "token.h"
+#include <stdio.h>
+
+//driver function to initialize the parser
+extern void init_lexer();
 
 // driver function to get next tokens from text
 //when called, it loads the current_token variable with the information of the next token in input file
